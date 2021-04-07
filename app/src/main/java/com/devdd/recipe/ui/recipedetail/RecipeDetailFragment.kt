5bfce118ab1e355c96f6cf7dcff430f0e0e2f7a5
Deepak Dawade <deepakdawade.dd1@gmail.com>
2021-04-08 00:13:01 +0530

    RecipeDetailFragment added

---
+++ app/src/main/java/com/devdd/recipe/ui/recipedetail/RecipeDetailFragment.kt
@@ -0,0 +1,25 @@
+package com.devdd.recipe.ui.recipedetail
+
+import android.os.Bundle
+import androidx.navigation.fragment.navArgs
+import com.devdd.recipe.R
+import com.devdd.recipe.base.MyFragment
+import com.devdd.recipe.databinding.FragmentRecipeDetailBinding
+import com.devdd.recipe.ui.home.viewstate.RecipeViewState
+import com.devdd.recipe.utils.extensions.toDataClass
+
+
+class RecipeDetailFragment :
+    MyFragment<FragmentRecipeDetailBinding>(R.layout.fragment_recipe_detail) {
+
+    companion object {
+        val TAG: String = this::class.java.simpleName
+    }
+
+    private val args by navArgs<RecipeDetailFragmentArgs>()
+
+    override fun onViewCreated(binding: FragmentRecipeDetailBinding, savedInstanceState: Bundle?) {
+        val recipe = args.recipe.toDataClass<RecipeViewState>()
+        binding.recipe = recipe
+    }
+}
