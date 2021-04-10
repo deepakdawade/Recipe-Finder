38a96f3dd67ae3d27ab8c1ea1133af8a154acac8
Deepak Dawade <deepakdawade.dd1@gmail.com>
2021-04-11 02:52:50 +0530

    ProfileFragment with change setting

---
+++ app/src/main/java/com/devdd/recipe/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,31 @@
+package com.devdd.recipe.ui.profile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavDirections
+import com.devdd.recipe.domain.result.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor() : ViewModel() {
+
+    private val mNavigation: MutableLiveData<Event<Pair<NavDirections, Boolean>>> =
+        MutableLiveData()
+    val navigation: LiveData<Event<Pair<NavDirections, Boolean>>>
+        get() = mNavigation
+
+
+    fun navigateToChangeLanguage() {
+        val direction = ProfileFragmentDirections.actionToPreferenceSettingFragment()
+        mNavigation.value = Event(Pair(direction, false))
+
+    }
+
+    fun navigateToChangeRecipePref() {
+        val direction = ProfileFragmentDirections.actionToPreferenceSettingFragment(1)
+        mNavigation.value = Event(Pair(direction, false))
+    }
+
+}
