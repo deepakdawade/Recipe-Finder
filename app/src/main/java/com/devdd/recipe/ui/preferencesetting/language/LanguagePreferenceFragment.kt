372e574daa9421917f877b5ba02d551221f3a10a
Deepak Dawade <deepakdawade.dd1@gmail.com>
2021-04-10 22:49:41 +0530

    annotation AndroidEntryPoint to fragment

--- app/src/main/java/com/devdd/recipe/ui/preferencesetting/language/LanguagePreferenceFragment.kt
+++ app/src/main/java/com/devdd/recipe/ui/preferencesetting/language/LanguagePreferenceFragment.kt
@@ -7,7 +7,9 @@ import com.devdd.recipe.R
 import com.devdd.recipe.base.DevFragment
 import com.devdd.recipe.databinding.FragmentLanguagePreferenceBinding
 import com.devdd.recipe.utils.extensions.observeEvent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LanguagePreferenceFragment :
     DevFragment<FragmentLanguagePreferenceBinding>(R.layout.fragment_language_preference) {
     companion object {
