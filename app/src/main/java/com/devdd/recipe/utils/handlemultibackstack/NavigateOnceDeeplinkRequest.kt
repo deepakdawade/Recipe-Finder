935a1953f65058ad99f7d1b3f22996c34982cbb2
Deepak Dawade <deepakdawade.dd1@gmail.com>
2021-04-11 07:30:43 +0530

    bottom navigation extension function added

---
+++ app/src/main/java/com/devdd/recipe/utils/handlemultibackstack/NavigateOnceDeeplinkRequest.kt
@@ -0,0 +1,18 @@
+package com.devdd.recipe.utils.handlemultibackstack
+
+import android.net.Uri
+
+data class NavigateOnceDeeplinkRequest(
+    // The deeplink of the destination
+    val deeplink: Uri,
+
+    // Defines the behavior of multiple continuous instances to be allowed or not for a destination.
+    // true: allows continuous instances of a same fragment.
+    // false: do not allow continuous instances of same fragment.
+    val allowMultipleInstance: Boolean = false,
+
+    //  if set to true fragment will be popped from backStack and navigate
+    // else set to false backStackEntry will be used with old arguments if any
+    val updateArguments: Boolean = true
+)
+ 
