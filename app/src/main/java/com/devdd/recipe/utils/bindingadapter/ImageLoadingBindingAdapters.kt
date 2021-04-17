c44e2a5878d3e327f683840a337e4fd9fdd3bfbf
Deepak Dawade <deepakdawade.dd1@gmail.com>
2021-04-17 19:45:58 +0530

    fixed card corners issue

--- app/src/main/java/com/devdd/recipe/utils/bindingadapter/ImageLoadingBindingAdapters.kt
+++ app/src/main/java/com/devdd/recipe/utils/bindingadapter/ImageLoadingBindingAdapters.kt
@@ -4,14 +4,29 @@ import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
 import com.devdd.recipe.R
+import com.devdd.recipe.utils.extensions.px
 
-@BindingAdapter("loadImage", "roundImage",requireAll = false)
-fun AppCompatImageView.loadImage(imageUrl: String?, isRoundImage: Boolean? = false) {
+@BindingAdapter(
+    "loadImage",
+    "circleCrop",
+    "roundCorner",
+    "cornerRadius",
+    requireAll = false
+)
+fun AppCompatImageView.loadImage(
+    imageUrl: String?,
+    circleCrop: Boolean? = false,
+    roundCorner: Boolean? = false,
+    cornerRadius: Int? = 0
+) {
     load(imageUrl) {
         placeholder(R.drawable.ic_launcher_foreground)
-        if (isRoundImage == true)
+        if (circleCrop == true)
             transformations(CircleCropTransformation())
+        else if (roundCorner == true)
+            transformations(RoundedCornersTransformation(radius = (cornerRadius ?: 0).px(context)))
         error(R.drawable.ic_launcher_foreground)
     }
 }
