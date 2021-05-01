19a0e599a704b9a6dfaaddd01b9f8b287d26d1b6
Deepak Dawade <deepakdawade.dd1@gmail.com>
2021-05-01 21:11:09 +0530

    deleted CategoryViewState.kt

--- app/src/main/java/com/devdd/recipe/domain/viewstate/CategoryViewState.kt
+++
@@ -1,8 +0,0 @@
-package com.devdd.recipe.domain.viewstate
-
-data class CategoryViewState(
-    val id: Int = 0,
-    val name: String = "",
-    val description: String = "",
-    val imageUrl: String = "",
-)
