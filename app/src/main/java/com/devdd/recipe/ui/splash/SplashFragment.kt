02ed3a4e3b4703c204c704619d3a3de60e14d4f8
Deepak Dawade <Deepakdawade.dd1@gmail.com>
2021-06-15 01:03:52 +0530

    feat: removed splash animation

--- app/src/main/java/com/devdd/recipe/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/devdd/recipe/ui/splash/SplashFragment.kt
@@ -1,17 +1,16 @@
 package com.devdd.recipe.ui.splash
 
-import android.animation.Animator
 import android.os.Bundle
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.devdd.recipe.R
-import com.devdd.recipe.ui.base.DevFragment
-import com.devdd.recipe.databinding.FragmentSplashBinding
 import com.devdd.recipe.base_android.utils.extensions.observeEvent
+import com.devdd.recipe.databinding.FragmentSplashBinding
+import com.devdd.recipe.ui.base.DevFragment
 import com.devdd.recipe.utils.extensions.navigateOnce
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class SplashFragment : DevFragment<FragmentSplashBinding>(R.layout.fragment_splash) {
@@ -22,20 +21,11 @@ class SplashFragment : DevFragment<FragmentSplashBinding>(R.layout.fragment_spla
     private val viewModel by viewModels<SplashViewModel>()
 
     override fun onViewCreated(binding: FragmentSplashBinding, savedInstanceState: Bundle?) {
-        binding.splashFragmentLottieAnimation.addAnimatorListener(object :
-            Animator.AnimatorListener {
-            override fun onAnimationRepeat(animation: Animator?) {}
 
-            override fun onAnimationEnd(animation: Animator?) {
-                lifecycleScope.launch {
-                    viewModel.decideDestination()
-                }
-            }
-
-            override fun onAnimationCancel(animation: Animator?) {}
-
-            override fun onAnimationStart(animation: Animator?) {}
-        })
+        lifecycleScope.launchWhenResumed {
+            delay(2000)
+            viewModel.decideDestination()
+        }
 
         viewModel.navigation.observeEvent(viewLifecycleOwner) {
             findNavController().navigateOnce(it)
